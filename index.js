51f2fc589b0ee9226c56c62f25459745db94f363
suhani tamrkar <suhanitamrkar2004@gmail.com>
2024-10-28 09:03:23 +0530

    learn classes in javascript

---
+++ index.js
@@ -0,0 +1,36 @@
+console.log("suhani");
+// classes in javasript
+// class Car {
+//   constructor(name, year) {
+//     this.name = name;
+//     this.year = year;
+//   }
+//   //method
+// //   age() {
+// //     const date = new Date();
+// //     return date.getFullYear() - this.year;
+// //   }
+// age(x){
+//     return x - this.year
+// }
+// }
+// const date = new Date()
+// let year = date.getFullYear()
+// const myCar = new Car("BMW", 2014);
+// document.getElementById("demo").innerHTML =
+//   "My car is " + myCar.age(year) + "years old";//My car is 10years old
+// const myCar1 = new Car("Ford", 2013);
+// const myCar2 = new Car("Audi", 2014);
+// class Car {
+//     constructor (brand){
+//         this.carname =brand
+//     }
+//     get cnam(){
+//         return this.carname
+//     }
+//     set cname(x){
+//         this.carname = x
+//     }
+// }
+// const myCar = new Car('Ford')
+// document.getElementById('demo').innerHTML = myCar.cnam
